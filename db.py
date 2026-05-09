3deb082271c935421afa1cea3c4c1f1e0f6826da
LeandroSiqueira88 <leandro_cs00@hotmail.com>
2026-05-09 17:29:54 -0300

    Adiciona  db.py

--- db.py
+++ db.py
@@ -6,7 +6,6 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 def get_db_connection():
     if DATABASE_URL:
         import psycopg2
-        import psycopg2.extras
         conn = psycopg2.connect(DATABASE_URL)
         conn.autocommit = False
         return conn
@@ -19,8 +18,4 @@ def is_postgres():
     return bool(DATABASE_URL)
 
 def placeholder():
-    """Retorna %s para postgres ou ? para sqlite"""
     return '%s' if is_postgres() else '?'
-
-
-### 
